641a400aff11c18ed020a91654e313216a9eae31
peter14067 <peter14067@gmail.com>
2020-04-11 16:10:23 +0800

    Updates

---
+++ precache-manifest.6e678daf65b31419bc1adc225b73d65d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f644504e3b951d2b31d2bfd734a6cdb1",
+    "url": "/chatroom/index.html"
+  },
+  {
+    "revision": "2f8147c82ee725721acf",
+    "url": "/chatroom/static/js/2.ced6539a.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/chatroom/static/js/2.ced6539a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5170dc402f8becaeb055",
+    "url": "/chatroom/static/js/main.a7aebfa3.chunk.js"
+  },
+  {
+    "revision": "820bf7a8317b27be265f",
+    "url": "/chatroom/static/js/runtime-main.e099b263.js"
+  }
+]);
